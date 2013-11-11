58df98220d639492dbfab5bb41e2041b4da3021d
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-11 11:17:14 +0200

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject iloveponies.tests/p-p-p-pokerface "0.1.0-SNAPSHOT"
+  :description "tests for p-p-p-pokerface"
+  :url "http://github.com/iloveponies/p-p-p-pokerface"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.1"]
+                 [midje-grader "0.1.0-SNAPSHOT"]])
