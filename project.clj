56e577d3d5ca241e66fef5e77241771a57d58bfe
Ilmo Raunio <ilmo.raunio@gmail.com>
2019-06-24 18:35:14 +0300

    Add support for latest jdk (11)

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject iloveponies.tests/p-p-p-pokerface "0.1.0-SNAPSHOT"
+(defproject iloveponies.tests/p-p-p-pokerface "0.2.0-SNAPSHOT"
   :description "tests for p-p-p-pokerface"
   :url "http://github.com/iloveponies/p-p-p-pokerface"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [midje "1.9.4"]
                  [midje-grader "0.1.0-SNAPSHOT"]])
